dc26a682a051c9e652bdd88ba3a2cc8813ee7786
christiful <christiful@live.cn>
2015-04-21 16:44:57 +0800

    Create SolutionList.java

---
+++ SolutionList.java
@@ -0,0 +1,15 @@
+public class SolutionList {
+		
+	public void add(Solution s){
+		
+	}
+	public void delete(Solution s){
+		
+	}
+	public Solution search(Spot begin, Spot end){
+		return ;
+	}
+	public boolean empty(){
+		return false;
+	}
+}
